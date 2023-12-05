d9c5b877bbc3ee296362e2f40d0e6ee04179dfbc
qadar omar <omarqadar3@gmail.com>
2023-12-04 21:22:28 -0800

    competed best books fetch and added api key to separate file

---
+++ api-key.js
@@ -0,0 +1,1 @@
+const API_KEY = "FIW2P8fg2UWNZ235VGwRGx0lmTQkh007";
--- best-books.js
+++ best-books.js
@@ -1,14 +1,56 @@
-const formEl = document.getElementById('best-books-form');
-const yearEl = document.getElementById('year');
-const monthEl = document.getElementById('month');
-const dateEl = document.getElementById('date');
+const formEl = document.getElementById("best-books-form");
+const yearEl = document.getElementById("year");
+const monthEl = document.getElementById("month");
+const dayEl = document.getElementById("date");
 
-formEl.addEventListener('submit', function(e) {
+//Fetch bestselling books for date and add top 5 to page
+//2019-01-20/hardcover-fiction.json
+
+formEl.addEventListener("submit", function (e) {
   e.preventDefault();
+  const date = `${yearEl.value}-${monthEl.value}-${dayEl.value}`;
+  const list = "hardcover-fiction";
 
-  const year = yearEl.value;
-  const month = monthEl.value;
-  const date = dateEl.value;
+  const apiUrl = `https://api.nytimes.com/svc/books/v3/lists/${date}/${list}.json?api-key=${API_KEY}`;
 
-  // Fetch bestselling books for date and add top 5 to page
+  fetch(apiUrl)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (responseJson) {
+      console.log(responseJson);
+
+      let bookArray = responseJson.results.books;
+      console.log(bookArray);
+
+      // Clear out the old content and 5 books
+      const bookContainer = document.getElementById("books-container");
+      bookContainer.innerHTML = "";
+
+      let ul = document.createElement("ul");
+      let textContent = "Best Selling Hardcover Fiction Books";
+      ul.append(textContent);
+      bookContainer.appendChild(ul);
+
+      // loop bookArray for top 5
+      for (let i = 0; i < 5; i++) {
+        let li = document.createElement("li");
+        let h2 = document.createElement("h2");
+        let address = document.createElement("address");
+        let p = document.createElement("p");
+
+        h2.textContent = bookArray[i].title;
+        address.textContent = bookArray[i].author;
+        p.textContent = bookArray[i].description;
+
+        li.appendChild(h2);
+        li.appendChild(address);
+        li.appendChild(p);
+
+        ul.appendChild(li);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 });
